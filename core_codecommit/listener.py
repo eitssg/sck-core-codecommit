70b94678e75f8ed466ee1b2b9ea04d90dcf064b8
James Barwick <jbarwick@me.com>
2024-12-28 21:56:07 +0800

    Version 0.0.3

--- core_codecommit/listener.py
+++ core_codecommit/listener.py
@@ -3,143 +3,154 @@ CI lambda for core-* repos that use the deployspec compiler.
 AWS_PROFILE=automation AWS_REGION=ap-southeast-1 ./test.py
 """
 
-import os
-import re
 import boto3
 import time
+import traceback
+import core_logging as log
 
-from core_api import get_facts
+import core_framework as util
+from core_framework.models import DeploymentDetails
+
+from ._version import __version__
 
 
-def __get_deployment_details(event):
-    record = event["Records"][0]
+def __get_deployment_details(record: dict) -> DeploymentDetails:
+    """
+    Translate the codecommit event Record into a DeploymentDetails object.
+
+    Args:
+        record (dict): _description_
+
+    Returns:
+        DeploymentDetails: The deployment details to pass to CodeDeploy
+    """
     # Dashes in repo names can break the first solution.
     # portfolio, app = record['eventSourceARN'].split(':')[-1].split('-')
     parts = record["eventSourceARN"].split(":")[-1].split("-")
     portfolio = parts[0]
     app = "-".join(parts[1:])
 
-    branch = record["codecommit"]["references"][0]["ref"].split("/")[-1]
-    # FIXME Build is always 1 for core-* repos
-    # build = record['codecommit']['references'][0]['commit']  # FIXME SSM param store for build numbers?
-    build = "1"
-    return {"portfolio": portfolio, "app": app, "branch": branch, "build": build}
+    references = record["codecommit"]["references"][0]
+    branch = references["ref"].split("/")[-1]
+    build = references["commit"][:7]
+
+    return DeploymentDetails(
+        Portfolio=portfolio,
+        App=app,
+        Branch=branch,
+        Build=build,
+    )
 
 
-def __s3_path_format(name):
-    """Helper function."""
-    return re.sub("_", "-", name)
-
-
-def __get_identity(deployment_details):
-    """
-    Copied from lambdas/invoker/main.py, case is different :(
-    """
-    return ":".join(
-        [
-            "prn",
-            deployment_details["portfolio"],
-            deployment_details["app"],
-            deployment_details["branch"],  # FIXME branchshortname?
-            deployment_details["build"],
-        ]
-    ).lower()
-
-
-def __get_new_build_number(deployment):
-    client = boto3.client("ssm")
+def __get_new_build_number(deployment: DeploymentDetails) -> str:
 
     param_name = "/{}/{}/{}/build_time".format(
-        deployment["portfolio"], deployment["app"], deployment["branch"]
+        deployment.Portfolio, deployment.App, deployment.BranchShortName
     )
     current_time = str(int(round(time.time() * 1000)))
 
+    client = boto3.client("ssm")
     response = client.put_parameter(
         Name=param_name, Value=current_time, Type="String", Overwrite=True
     )
-    print(
-        "__get_new_build_number param_name={}, current_time={}, response={}".format(
+    log.info(
+        "New build number param_name={}, current_time={}, response={}".format(
             param_name, current_time, response
         )
     )
-    return response["Version"]
+    return str(response["Version"])
 
 
-def __invoke_codebuild_project(
-    deployment,
-    automation_region,
-    automation_bucket_name,
-    run_sh_s3_path,
-    client_name,
-    invoker_branch,
-):
-    """Invoke a codebuild project setup for the app.
-    http://boto3.readthedocs.io/en/docs/reference/services/codebuild.html#CodeBuild.Client.start_build
+def invoke_codebuild_project(dd: DeploymentDetails) -> dict:
     """
-    project_name = "{}-{}".format(deployment["portfolio"], deployment["app"])
-    branch = deployment["branch"]
-    new_build_number = __get_new_build_number(deployment)
+    Invoke codebuild project for the app.
+
+    See more details at: http://boto3.readthedocs.io/en/docs/reference/services/codebuild.html#CodeBuild.Client.start_build
+
+    Args:
+        dd (DeploymentDetails): The deployment details
+
+    Returns:
+        dict: The response from the codebuild project execution
+
+    """
+    project_name = "{}-{}".format(dd.Portfolio, dd.App)
+    build_number = __get_new_build_number(dd)
+    automation_bucket_name = util.get_bucket_name()
+    automation_region = util.get_region()
+
+    log.info("Initiating build for project: {}".format(project_name))
 
     env_vars = [
-        {"name": "CLIENT", "value": client_name, "type": "PLAINTEXT"},
-        {"name": "PORTFOLIO", "value": deployment["portfolio"], "type": "PLAINTEXT"},
-        {"name": "APP", "value": deployment["app"], "type": "PLAINTEXT"},
-        {"name": "BRANCH", "value": deployment["branch"], "type": "PLAINTEXT"},
+        {"name": "CLIENT", "value": dd.Client, "type": "PLAINTEXT"},
+        {"name": "PORTFOLIO", "value": dd.Portfolio, "type": "PLAINTEXT"},
+        {"name": "APP", "value": dd.App, "type": "PLAINTEXT"},
+        {"name": "BRANCH", "value": dd.Branch, "type": "PLAINTEXT"},
         # core-* repos are always build 1 - there's no blue/green for the foundations, just stack updates.
-        {"name": "BUILD", "value": "1", "type": "PLAINTEXT"},
-        {"name": "BUILD_NUMBER", "value": str(new_build_number), "type": "PLAINTEXT"},
+        {"name": "BUILD", "value": dd.Build, "type": "PLAINTEXT"},
+        {"name": "BUILD_NUMBER", "value": build_number, "type": "PLAINTEXT"},
         # Env var set in deploy.sh
         {"name": "BUCKET_NAME", "value": automation_bucket_name, "type": "PLAINTEXT"},
-        {"name": "RUN_SH_S3_PATH", "value": run_sh_s3_path, "type": "PLAINTEXT"},
-        {"name": "INVOKER_BRANCH", "value": invoker_branch, "type": "PLAINTEXT"},
     ]
 
-    print(
-        "__invoke_codebuild_project client_name={}, project_name={}, branch={}, env_vars={}".format(
-            client_name, project_name, branch, env_vars
-        )
-    )
+    log.info("Details of Environment:", details=env_vars)
 
     boto3_client = boto3.client("codebuild", region_name=automation_region)
+
     response = boto3_client.start_build(
         projectName=project_name,
-        sourceVersion=deployment["branch"],
+        sourceVersion=dd.Branch,
         environmentVariablesOverride=env_vars,
     )
 
-    print("__invoke_codebuild_project response={}".format(response))
+    log.info("response: ", details=response)
+
     return response
 
 
-def handler(event, context):
-    print("*** Handler running")
-    print("event={}".format(event))
+def handler(event: dict, context) -> dict:
+    """
+    Handle the event from the CodeCommit trigger.
 
-    automation_region = "ap-southeast-1"  # FIXME Set via env vars in deploy.sh etc.
-    deployment = __get_deployment_details(event)
-    print("deployment={}".format(deployment))
+    Args:
+        event (dict): _description_
+        context (dict): The context object
 
-    # Get app facts
-    identity = __get_identity(deployment)
+    Returns:
+        dict: The response to provide to the caller
+    """
 
-    print("identity={}".format(identity))
+    try:
 
-    facts = get_facts(**identity)
+        log.info("Commit Listenr Event v{}", __version__)
+        log.debug("event={}".format(event))
 
-    invoker_branch = facts.get("InvokerBranch", "master")
+        if "Records" not in event:
+            log.error("No 'Records' key in event")
+            raise ValueError("No 'Records' key in event")
 
-    # Trust in your codecommit triggers for only master.
-    # if deployment['portfolio'] == 'core' and deployment['branch'] != 'master':
-    #     raise ValueError('Branch MUST be master for core-* repos. Aborting.')
+        records = event["Records"]
 
-    response = __invoke_codebuild_project(
-        deployment,
-        automation_region,
-        os.environ["AUTOMATION_BUCKET_NAME"],
-        os.environ["RUN_SH_S3_PATH"],
-        os.environ["CLIENT_NAME"],
-        invoker_branch,
-    )
+        log.info("Processing {} Records".format(len(records)))
 
-    print("Done")
-    return {"id": response["build"]["id"]}
+        responses: list[dict] = []
+        for record in records:
+
+            dd = __get_deployment_details(record)
+
+            log.info("deployment: ", details=dd.model_dump())
+
+            identity = dd.get_identity()
+
+            log.info("identity={}", identity)
+
+            response = invoke_codebuild_project(dd)
+
+            responses.append(response)
+
+        return {"Responses": responses}
+
+    except Exception as e:
+        traceback.print_exc()
+        print("Error: {}".format(e))
+        raise e
--- tests/test_listener.py
+++ tests/test_listener.py
@@ -1,6 +1,145 @@
+from unittest.mock import patch
 import pytest
 
+from core_codecommit.listener import handler
 
-def test_codecommit_listener():
 
-    assert True
+start_build_response = {
+    "build": {
+        "id": "my-build-id",
+        "arn": "arn:aws:codebuild:us-west-2:123456789012:build/my-project:my-build-id",
+        "startTime": "2023-10-01T12:34:56Z",
+        "currentPhase": "SUBMITTED",
+        "buildStatus": "IN_PROGRESS",
+        "sourceVersion": "abcdef1234567890abcdef1234567890abcdef12",
+        "resolvedSourceVersion": "abcdef1234567890abcdef1234567890abcdef12",
+        "projectName": "my-project",
+        "phases": [
+            {
+                "phaseType": "SUBMITTED",
+                "phaseStatus": "SUCCEEDED",
+                "startTime": "2023-10-01T12:34:56Z",
+                "endTime": "2023-10-01T12:35:00Z",
+            },
+            {
+                "phaseType": "QUEUED",
+                "phaseStatus": "IN_PROGRESS",
+                "startTime": "2023-10-01T12:35:00Z",
+            },
+        ],
+        "source": {
+            "type": "CODECOMMIT",
+            "location": "https://git-codecommit.us-west-2.amazonaws.com/v1/repos/my-repo",
+        },
+        "artifacts": {"location": "arn:aws:s3:::my-artifact-bucket/my-artifact.zip"},
+        "environment": {
+            "type": "LINUX_CONTAINER",
+            "image": "aws/codebuild/standard:4.0",
+            "computeType": "BUILD_GENERAL1_SMALL",
+            "environmentVariables": [
+                {"name": "ENV_VAR_NAME", "value": "value", "type": "PLAINTEXT"}
+            ],
+        },
+        "logs": {
+            "groupName": "/aws/codebuild/my-project",
+            "streamName": "my-build-id",
+            "deepLink": "https://console.aws.amazon.com/cloudwatch/home?region=us-west-2#logEvent:group=/aws/codebuild/my-project;stream=my-build-id",
+        },
+    }
+}
+
+
+class MagicClient:
+
+    def put_parameter(self, **kwargs):
+
+        param_name = kwargs.get("Name")
+        current_time = kwargs.get("Value")
+        type = kwargs.get("Type")
+        overwrite = kwargs.get("Overwrite")
+
+        assert param_name == "/portfolio/my-repo/main/build_time"
+        assert current_time is not None
+        assert type == "String"
+        assert overwrite is True
+
+        return {"Version": "param_store_id"}
+
+    def start_build(self, **kwargs):
+
+        project_name = kwargs.get("projectName")
+        source_version = kwargs.get("sourceVersion")
+        env_vars = kwargs.get("environmentVariablesOverride")
+
+        assert project_name == "portfolio-my-repo"
+        assert source_version == "main"
+        assert env_vars == [
+            {"name": "CLIENT", "value": "test", "type": "PLAINTEXT"},
+            {"name": "PORTFOLIO", "value": "portfolio", "type": "PLAINTEXT"},
+            {"name": "APP", "value": "my-repo", "type": "PLAINTEXT"},
+            {"name": "BRANCH", "value": "main", "type": "PLAINTEXT"},
+            {"name": "BUILD", "value": "abcdef1", "type": "PLAINTEXT"},
+            {"name": "BUILD_NUMBER", "value": "param_store_id", "type": "PLAINTEXT"},
+            {
+                "name": "BUCKET_NAME",
+                "value": "test-core-automation-master",
+                "type": "PLAINTEXT",
+            },
+        ]
+
+        return start_build_response
+
+
+@pytest.fixture
+def codecommit_listener():
+
+    with patch("boto3.client") as mock_client:
+        mock_client.return_value = MagicClient()
+        yield mock_client
+
+
+@pytest.fixture
+def event():
+
+    return {
+        "Records": [
+            {
+                "eventId": "12345678-1234-1234-1234-123456789012",
+                "eventVersion": "1.0",
+                "eventTime": "2023-10-01T12:34:56Z",
+                "eventSource": "aws:codecommit",
+                "awsRegion": "us-west-2",
+                "eventName": "ReferenceChanges",
+                "userIdentityARN": "arn:aws:iam::123456789012:user/username",
+                "eventSourceARN": "arn:aws:codecommit:us-west-2:123456789012:portfolio-my-repo",
+                "repositoryId": "12345678-1234-1234-1234-123456789012",
+                "codecommit": {
+                    "references": [
+                        {
+                            "ref": "refs/heads/main",
+                            "commit": "abcdef1234567890abcdef1234567890abcdef12",
+                        }
+                    ]
+                },
+            }
+        ]
+    }
+
+
+def test_codecommit_listener(codecommit_listener, event):
+
+    try:
+        response = handler(event, None)
+
+        assert response is not None
+
+        assert "Responses" in response
+
+        responses = response["Responses"]
+
+        response = responses[0]
+
+        assert response["build"]["buildStatus"] == "IN_PROGRESS"
+
+    except Exception as e:
+        assert False, e
